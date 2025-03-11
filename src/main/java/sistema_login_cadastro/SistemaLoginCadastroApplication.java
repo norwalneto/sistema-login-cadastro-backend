ee0f4366611c3dd8e825568a86c5851a5551f811
norwalneto <norwalgomess@gmail.com>
2025-03-11 00:36:58 -0300

    Primeiro commit

---
+++ src/main/java/sistema_login_cadastro/SistemaLoginCadastroApplication.java
@@ -0,0 +1,13 @@
+package sistema_login_cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaLoginCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaLoginCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sistema_login_cadastro/SistemaLoginCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema_login_cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLoginCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
