6d646133bd97fa8135dc8c063a9bbef16a7aea4b
norwalneto <norwalgomess@gmail.com>
2025-03-12 17:02:36 -0300

    atualização do projeto

--- src/main/java/sistema_login_cadastro/domain/repository/UserRepository.java
+++ src/main/java/sistema_login_cadastro/domain/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package sistema_login_cadastro.domain.reposiory;
+package sistema_login_cadastro.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +8,5 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existeByEmail(String email);
 }
